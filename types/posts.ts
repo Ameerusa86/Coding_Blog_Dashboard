53640a6d653036c255dd84ce90a164c7b4510bcf
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-07-08 19:59:01 -0400

    Update Post interface to include comments

--- types/posts.ts
+++ types/posts.ts
@@ -1,13 +1,14 @@
-// src/types/posts.ts
 export interface Post {
   id: string;
   title: string;
-  description: string;
+  body: string;
   author: string;
-  category: string;
-  technologies: string[];
-  githubLink: string;
   date: string;
-  image: string;
-  createdAt: string;
+  comments: PostComment[];
+}
+
+export interface PostComment {
+  id: string;
+  text: string;
+  username: string;
 }
